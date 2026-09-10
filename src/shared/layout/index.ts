c416ac9c18829337167935b0a7c4c6ef92783edd
IMRD <imrd.works@yandex.ru>
2026-09-10 11:49:45 +0300

    refactor: remove unused shared ui and helper modules

--- src/shared/layout/index.ts
+++ src/shared/layout/index.ts
@@ -1,4 +1,1 @@
-export { default as Section } from "./Section/Section.vue";
 export { default as Container } from "./Container/Container.vue";
-export { default as Grid } from "./Grid/Grid.vue";
-export { default as Box } from "./Box/Box.vue";
--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,11 +1,3 @@
 export { default as Button } from "./Button/Button.vue";
-export { default as Card } from "./Card/Card.vue";
-export { default as CardSkeleton } from "./Card/CardSkeleton.vue";
 export { default as Icon } from "./Icon/Icon.vue";
-export { default as Input } from "./Input/Input.vue";
-export { default as Modal } from "./Modal/Modal.vue";
 export { default as Motion } from "./Motion/Motion.vue";
-export { default as Select } from "./Select/Select.vue";
-export { default as Text } from "./Text/Text.vue";
-
-export type { SelectOption } from "./Select/Select.vue";
